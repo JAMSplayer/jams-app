6d9da6f28187cf6b54f802ccac1ed4e3f3472472
cobite <chainwizard@proton.me>
2024-10-31 21:47:48 +0000

    Adding register UI
    Adding register UI

--- src/components/account/AccountConnect.tsx
+++ src/components/account/AccountConnect.tsx
@@ -1,13 +1,14 @@
 import { Button } from "../ui/button";
 import { useState } from "react";
-import { Copy, PowerIcon } from "lucide-react";
+import { Copy, PowerIcon, UserRoundPlusIcon } from "lucide-react";
 import Avatar from "./Avatar";
 import { copyToClipboard } from "@/lib/utils/clipboard";
 import { formatAddress } from "@/lib/utils/address";
 import { formatBalance } from "@/lib/utils/balance";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 export default function AccountConnect() {
-    const isConnected = true; // TODO update to use the new hook
     const address = "0x3153176c72100b45bdA3A312E5d2fe12a1806a7A"; // TODO update to use the new hook
     const username = "Dirvine"; // TODO update to use the new hook
     // TODO get this from the to be created hook by loziniak
@@ -26,14 +27,27 @@ export default function AccountConnect() {
         <></>
     );
 
-    const [infoPanelOpen, setInfoPanelOpen] = useState(false);
+    const [isConnected, setIsConnected] = useState(true); // TODO update to use the new hook
+    const [isConnectedPanelOpen, setIsConnectedPanelOpen] = useState(false);
+    const [isSignInPanelOpen, setIsSignInPanelOpen] = useState(false);
 
     const disconnect = () => {
         //  TODO functionality to disconnect - this can be created via tauri commands to the backend.
+        setIsConnected(false);
+        setIsConnectedPanelOpen(false);
     };
 
     const connect = () => {
         //  TODO functionality to connect - this can be created via tauri commands to the backend.
+        setIsSignInPanelOpen(!isSignInPanelOpen);
+    };
+
+    const signIn = () => {
+        //  TODO
+    };
+
+    const addAccount = () => {
+        //  TODO
     };
 
     return (
@@ -44,20 +58,20 @@ export default function AccountConnect() {
                         <div
                             className="cursor-pointer"
                             onClick={() => {
-                                setInfoPanelOpen(!infoPanelOpen);
+                                setIsConnectedPanelOpen(!isConnectedPanelOpen);
                             }}
                         >
                             <Avatar address={address} />
                         </div>
                     )}
 
-                    {infoPanelOpen && (
-                        <div className="">
-                            <div className="absolute right-3 mt-4 w-73 origin-top-right rounded-lg bg-card shadow-large  border z-50">
+                    {isConnectedPanelOpen && (
+                        <>
+                            <div className="absolute right-3 mt-4 w-73 origin-top-right rounded-lg bg-card shadow-large border z-50">
                                 <div className="border-b border-dashed px-4 py-5 border-secondary">
                                     {username}
-                                    <div className="flex w-full mt-3 ">
-                                        <div className="flex-grow rounded-lg bg-secondary px-2 py-1 text-sm tracking-tighter ">
+                                    <div className="flex w-full mt-3">
+                                        <div className="flex-grow rounded-lg bg-secondary px-2 py-1 text-sm tracking-tighter">
                                             {address && formatAddress(address)}
                                         </div>
 
@@ -95,17 +109,80 @@ export default function AccountConnect() {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </>
                     )}
                 </>
             ) : (
-                <Button
-                    variant={"default"}
-                    size={"sm"}
-                    onClick={() => connect()}
-                >
-                    Connect Account
-                </Button>
+                <>
+                    <Button
+                        variant={"default"}
+                        size={"sm"}
+                        onClick={() => connect()}
+                    >
+                        Connect Account
+                    </Button>
+
+                    {isSignInPanelOpen && (
+                        <>
+                            <div className="absolute right-3 mt-4 w-73 origin-top-right rounded-lg bg-card shadow-large border z-50">
+                                <div className="border-b border-dashed px-4 py-5 border-secondary">
+                                    <form className="flex flex-col space-y-4">
+                                        <div className="flex items-center">
+                                            <Label
+                                                htmlFor="username"
+                                                className="mr-2"
+                                            >
+                                                Username
+                                            </Label>
+                                            <Input
+                                                id="username"
+                                                type="text"
+                                                placeholder="Enter your username"
+                                                className="flex-1"
+                                            />
+                                        </div>
+
+                                        <div className="flex items-center">
+                                            <Label
+                                                htmlFor="password"
+                                                className="mr-2"
+                                            >
+                                                Password
+                                            </Label>
+                                            <Input
+                                                id="password"
+                                                type="password"
+                                                placeholder="Enter your password"
+                                                className="flex-1"
+                                            />
+                                        </div>
+
+                                        <Button
+                                            onClick={() => signIn()}
+                                            className="mt-4"
+                                        >
+                                            Sign In
+                                        </Button>
+                                    </form>
+                                </div>
+
+                                <div className="p-3 flex justify-center">
+                                    <div
+                                        className="flex cursor-pointer items-center justify-center gap-3 rounded-lg px-3 py-2.5 text-sm font-medium transition hover:bg-secondary w-full"
+                                        onClick={() => addAccount()}
+                                    >
+                                        <div className="flex items-center justify-center">
+                                            <UserRoundPlusIcon />
+                                        </div>
+                                        <span className="uppercase text-center">
+                                            Add Account
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+                </>
             )}
         </div>
     );
