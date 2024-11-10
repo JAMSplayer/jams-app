bf30f8497e21b912dbd33dd6f59e94eed93b09ba
cobite <chainwizard@proton.me>
2024-11-10 08:31:23 +0000

    <div can't be a descendant of <p
    <div can't be a descendant of <p

--- src/components/admin-panel/footer.tsx
+++ src/components/admin-panel/footer.tsx
@@ -18,7 +18,7 @@ export function Footer() {
     return (
         <div className="relative w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="mx-4 md:mx-8 flex h-14 items-center justify-between">
-                <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
+                <div className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
                     <AnimatePresence>
                         {!isPlayerVisible && (
                             <motion.div
@@ -55,7 +55,7 @@ export function Footer() {
                         JAMS
                     </a>
                     .
-                </p>
+                </div>
             </div>
             <AnimatePresence>
                 {isPlayerVisible && (
