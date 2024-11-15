9696759eeefce50d4ced317abe84b284a1607190
loziniak <maciej@robotix-lozinski.pl>
2024-11-15 15:12:04 +0100

    Update to proper RegisterAddress in dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -6,8 +6,8 @@ export default function Dashboard() {
             <SongLoadButton
                 song={{
                     location:
-                        "http://localhost:12345/3509bad03dc869dec883c7b44662c3503d2517fa9e828bb64f4dbe719d3837bf__BegBlag.mp3",
-                    title: "song-1",
+                        "http://localhost:12345/08dbb205f5a5712e48551c0e437f07be304a5daadf20e07e8307e7f564fa9962823aacdc081a17136c4e09f82a29ac50dba22dbc898a41b5d68d4971dc9b62ad5d82ef0e5f9d7b2224eb285497489d4a__BegBlag.mp3",
+                    title: "BegBlag",
                 }}
             />
         </div>
