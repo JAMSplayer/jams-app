d6a9dee97ff260c22c9c24aaa7dd9aa982053e68
loziniak <maciej@robotix-lozinski.pl>
2024-11-26 22:41:16 +0100

    Get avatar.tsx and dashboard.tsx back

---
+++ src/components/dashboard.tsx
@@ -0,0 +1,20 @@
+import { SongLoadButton } from "./player/song-load-button";
+
+export default function Dashboard() {
+    return (
+        <div className="p-4 space-x-2">
+            <SongLoadButton
+                song={{
+                    location:
+                        "http://localhost:12345/08dbb205f5a5712e48551c0e437f07be304a5daadf20e07e8307e7f564fa9962823aacdc081a17136c4e09f82a29ac50dba22dbc898a41b5d68d4971dc9b62ad5d82ef0e5f9d7b2224eb285497489d4a__BegBlag.mp3",
+                    title: "BegBlag",
+                    artist: "BegBlag",
+                    album: "undefined",
+                    genre: "undefined",
+                    year: "undefined",
+                    tags: ["undefined"],
+                }}
+            />
+        </div>
+    );
+}
