3abb54334504e5fb0f5eafa7442e64dc218fdb3e
cobite <chainwizard@proton.me>
2024-11-27 12:48:12 +0000

    disconnected-panel design
    disconnected-panel design

--- src/components/disconnected-panel.tsx
+++ src/components/disconnected-panel.tsx
@@ -1,19 +1,23 @@
 import { connect } from "@/backend/autonomi";
 import { Button } from "./ui/button";
+import { GlobeLockIcon, ZapOffIcon } from "lucide-react";
 
 export default function DisconnectedPanel() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-            <p className="mt-6 text-pretty text-lg font-medium sm:text-xl/8 text-center">
+            <ZapOffIcon size={50} />
+            <p className="mt-8 text-pretty text-lg font-medium sm:text-xl/8 text-center flex items-center justify-center gap-2">
                 You are disconnected
             </p>
-            <div className="mt-4">
+
+            <div className="mt-6">
                 <Button
                     onClick={() => {
                         connect();
                     }}
                 >
                     Connect to network
+                    <GlobeLockIcon />
                 </Button>
             </div>
         </div>
