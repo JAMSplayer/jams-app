c7a02c335aaad5c70e77a8f709db510f7c7166b4
cobite <chainwizard@proton.me>
2025-03-04 20:24:02 +0000

    redact download folders
    redact download folders

--- src/types/songs/song.ts
+++ src/types/songs/song.ts
@@ -1,16 +1,14 @@
 export type Song = {
     id: string;
     xorname: string;
-    location: string;
-
     title: string;
     artist: string;
-    trackNumber?: number;
-    picture?: string;
-    tags?: string[];
-
-    description?: string;
-
     dateCreated: Date;
     dateUpdated?: Date;
+    fileName: string;
+    extension: string;
+    downloadFolder?: string;
+    tags?: string[];
+    picture?: string;
+    trackNumber?: number;
 };
