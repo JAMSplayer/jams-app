16fe3adb78710b95ee4b40e3d795934a868a3ef6
cobite <chainwizard@proton.me>
2025-03-20 23:51:28 +0000

    update test song button
    update test song button

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -5,29 +5,41 @@ import { logLevel } from "@/backend/autonomi";
 import { registeredAccounts } from "@/backend/logic";
 import { resolveResource } from "@tauri-apps/api/path";
 import { extractFromFullPath } from "@/lib/utils/location";
+import { useStorage } from "@/providers/storage-provider";
 
 export default function Dashboard() {
     const { isConnected, account } = useConnection();
+
+    const { store } = useStorage();
 
     return (
         <div className="p-4 space-y-2">
             <div className="flex flex-row">
                 <SongLoadButton
                     song={async () => {
-                        const pathData = extractFromFullPath(
-                            await resolveResource(
-                                "resources/A_Lazy_Farmer_Boy_by_Buster_Carter_And_Preston_Young.mp3"
-                            )
-                        );
-                        console.log("pathData: ", pathData);
-                        const { fileName, extension, folderPath } = pathData;
+                        if (!store) {
+                            throw new Error("Store is not initialized.");
+                        }
+
+                        const defaultDownloadFolder = await store.get<{
+                            value: string;
+                        }>("download-folder");
+
+                        if (
+                            !defaultDownloadFolder ||
+                            !defaultDownloadFolder.value
+                        ) {
+                            throw new Error(
+                                "No default download folder found."
+                            );
+                        }
 
                         return {
                             id: "123",
                             xorname: "124",
-                            downloadFolder: folderPath,
-                            fileName,
-                            extension,
+                            downloadFolder: defaultDownloadFolder.value,
+                            fileName: "glass",
+                            extension: "mp4",
                             title: "BegBlag",
                             artist: "BegBlag",
                             dateCreated: new Date(),
@@ -47,9 +59,30 @@ export default function Dashboard() {
                 Get Accounts
             </Button>
             <div>
-                <Button onClick={async () => { await logLevel("ERROR") }}> log: ERROR </Button>
-                <Button onClick={async () => { await logLevel("INFO") }}> log: INFO </Button>
-                <Button onClick={async () => { await logLevel("TRACE") }}> log: TRACE </Button>
+                <Button
+                    onClick={async () => {
+                        await logLevel("ERROR");
+                    }}
+                >
+                    {" "}
+                    log: ERROR{" "}
+                </Button>
+                <Button
+                    onClick={async () => {
+                        await logLevel("INFO");
+                    }}
+                >
+                    {" "}
+                    log: INFO{" "}
+                </Button>
+                <Button
+                    onClick={async () => {
+                        await logLevel("TRACE");
+                    }}
+                >
+                    {" "}
+                    log: TRACE{" "}
+                </Button>
             </div>
             <div className="flex flex-col">
                 <div>
