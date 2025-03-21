53b1a499d2b7113359239401da4633d0061f2ccb
cobite <chainwizard@proton.me>
2025-03-21 15:28:38 +0000

    Show how many playlists exist
    Show how many playlists exist

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -5,9 +5,33 @@ import { logLevel } from "@/backend/autonomi";
 import { registeredAccounts } from "@/backend/logic";
 import { resolveResource } from "@tauri-apps/api/path";
 import { extractFromFullPath } from "@/lib/utils/location";
+import { useStorage } from "@/providers/storage-provider";
+import { useEffect, useState } from "react";
+import { Playlist } from "@/types/playlists/playlist";
 
 export default function Dashboard() {
     const { isConnected, account } = useConnection();
+    const { store } = useStorage();
+    const [playlists, setPlaylists] = useState<Playlist[]>([]);
+
+    if (!store) {
+        console.error("Store is not initialized.");
+        return;
+    }
+
+    useEffect(() => {
+        const fetchPlaylists = async () => {
+            try {
+                const result = await store.get<Playlist[]>("playlists");
+                setPlaylists(result || []);
+            } catch (error) {
+                console.error("Failed to fetch playlists:", error);
+                setPlaylists([]);
+            }
+        };
+
+        fetchPlaylists();
+    }, [store]);
 
     return (
         <div className="p-4 space-y-2">
@@ -47,9 +71,40 @@ export default function Dashboard() {
                 Get Accounts
             </Button>
             <div>
-                <Button onClick={async () => { await logLevel("ERROR") }}> log: ERROR </Button>
-                <Button onClick={async () => { await logLevel("INFO") }}> log: INFO </Button>
-                <Button onClick={async () => { await logLevel("TRACE") }}> log: TRACE </Button>
+                <Button
+                    onClick={async () => {
+                        await logLevel("ERROR");
+                    }}
+                >
+                    {" "}
+                    log: ERROR{" "}
+                </Button>
+                <Button
+                    onClick={async () => {
+                        await logLevel("INFO");
+                    }}
+                >
+                    {" "}
+                    log: INFO{" "}
+                </Button>
+                <Button
+                    onClick={async () => {
+                        await logLevel("TRACE");
+                    }}
+                >
+                    {" "}
+                    log: TRACE{" "}
+                </Button>
+            </div>
+            <div>
+                <Button
+                    onClick={async () => {
+                        console.log("playlists amount: ", playlists.length);
+                    }}
+                >
+                    {" "}
+                    Playlists{" "}
+                </Button>
             </div>
             <div className="flex flex-col">
                 <div>
