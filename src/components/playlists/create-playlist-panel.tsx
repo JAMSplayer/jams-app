00acb0fcad509172a9cda8b1deb2cfd2c8185b40
cobite <chainwizard@proton.me>
2025-04-16 16:13:48 +0100

    extra features
    - blur background & portal for delete modal
    - won't allow creation of playlists with existing titles
    - updated create & update playlist components logic
    - updated some colors for dark mode
    - allow tags for playlists

--- src/components/playlists/create-playlist-panel.tsx
+++ src/components/playlists/create-playlist-panel.tsx
@@ -19,28 +19,36 @@ import { Song } from "@/types/songs/song";
 import { ScrollArea } from "../ui/scroll-area";
 import { useNavigate } from "react-router-dom";
 import { usePlayerStore } from "@/store/player-store";
+import { useImageSelector } from "@/hooks/use-image-selector";
+import { TagInput } from "../tag-input";
 
 type FormSchema = z.infer<typeof createPlaylistSchema>;
 
 export default function CreatePlaylistPanel() {
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        formState: { errors, isValid },
-    } = useForm<FormSchema>({
+    const createPlaylistForm = useForm<z.infer<typeof createPlaylistSchema>>({
         resolver: zodResolver(createPlaylistSchema),
-        mode: "onChange",
+        mode: "onBlur",
         defaultValues: {
             title: undefined,
             description: undefined,
             picture: undefined,
+            tags: [],
         },
     });
+    const {
+        handleSubmit,
+        register,
+        getValues,
+        setValue,
+        watch,
+        formState: { isValid },
+    } = createPlaylistForm;
 
     const { store } = useStorage();
     const navigate = useNavigate();
     const { isPlayerVisible } = usePlayerStore();
+
+    const titleValue = watch("title");
 
     // add songs ----------------------------------------------------------------
 
@@ -100,147 +108,15 @@ export default function CreatePlaylistPanel() {
 
     // image ----------------------------------------------------------------
 
-    const [base64Picture, setBase64Picture] = useState<string>("");
+    const { selectedImage, handleImageSelect } = useImageSelector();
 
-    // The image the user selects if they wish to change album art
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
-
-    useEffect(() => {
-        const pictureValue = base64Picture || selectedImage;
-        if (pictureValue) {
-            setValue("picture", pictureValue); // Update the form value whenever base64Picture or selectedImage changes
-        }
-    }, [base64Picture, selectedImage, setValue]);
-
-    const handleImageSelect = async () => {
-        const MAX_FILE_SIZE = 1 * 1024 * 1024; // 1 MB
-
-        try {
-            const selectedFile = await open({
-                multiple: false,
-                filters: [
-                    {
-                        name: "Images",
-                        extensions: ["jpg", "jpeg", "png", "gif"],
-                    },
-                ],
-            });
-
-            if (!selectedFile) {
-                toast("File", {
-                    description: "No file selected.",
-                });
-                return;
-            }
-
-            // Read the file as binary
-            const fileBuffer = await readFile(selectedFile as string);
-
-            // Get extension
-            const fileExtension = selectedFile.split(".").pop()?.toLowerCase();
-
-            if (
-                !fileExtension ||
-                !["jpg", "jpeg", "png", "gif"].includes(fileExtension)
-            ) {
-                toast("Unsupported", {
-                    description:
-                        "Unsupported file format. Please upload a JPG, JPEG, PNG, or GIF file.",
-                });
-                return;
-            }
-
-            const extensionToMime: Record<string, string> = {
-                jpg: "image/jpeg",
-                jpeg: "image/jpeg",
-                png: "image/png",
-                gif: "image/gif",
-            };
-
-            // Validate the extension and find the MIME type
-            const mimeType = extensionToMime[fileExtension];
-
-            // Convert the binary file into a Blob
-            const fileBlob = new Blob([new Uint8Array(fileBuffer)], {
-                type: mimeType,
-            });
-
-            // Create a File object
-            const file = new File(
-                [fileBlob],
-                `image.${fileExtension || "unknown"}`
-            );
-
-            // Check file size
-            if (file.size > MAX_FILE_SIZE) {
-                toast("File Size", {
-                    description:
-                        "File size exceeds 1MB. Please select a smaller file.",
-                });
-                return;
-            }
-
-            // Convert Blob to Base64
-            const base64Image = await convertToBase64(fileBlob);
-            console.log("base64Image: ", base64Image);
-
-            // Update the image state
-            setSelectedImage(base64Image); // Updates the selected image
-            setBase64Picture(base64Image); // Updates the base64Picture state
-
-            // Update the form value
+    const handleImageUpload = () => {
+        handleImageSelect((base64Image) => {
             setValue("picture", base64Image);
-        } catch (error) {
-            console.error("Error processing the selected image:", error);
-            toast("Error Processing Image", {
-                description: "Error processing the selected image.",
-            });
-        }
+        });
     };
 
     // end image ----------------------------------------------------------------
-
-    // tags ----------------------------------------------------------------
-
-    const MAX_TAGS = 5;
-    const MAX_TAG_LENGTH = 20;
-
-    const [tags, setTags] = useState<string[]>([]);
-    const [tagInput, setTagInput] = useState<string>(""); // Input field for adding tags
-
-    const addTag = (e: React.FormEvent) => {
-        e.preventDefault();
-        const trimmedTag = tagInput.trim().toLowerCase();
-
-        // Regular expression to allow only letters and numbers
-        const isValidTag = /^[a-zA-Z0-9]+$/.test(trimmedTag);
-
-        // Only add tag if conditions are met
-        if (
-            isValidTag && // Tag must only contain letters and numbers
-            trimmedTag &&
-            trimmedTag.length <= MAX_TAG_LENGTH &&
-            !tags.includes(trimmedTag) &&
-            tags.length < MAX_TAGS
-        ) {
-            setTags([...tags, trimmedTag]);
-            setTagInput("");
-        } else if (!isValidTag) {
-            toast("Invalid Tag", {
-                description: "Tags can only contain letters and numbers.",
-            });
-        } else if (trimmedTag.length > MAX_TAG_LENGTH) {
-            toast("Tag Length", {
-                description: `Tags must be ${MAX_TAG_LENGTH} characters or less.`,
-            });
-        }
-    };
-
-    const removeTag = (tag: string) => {
-        setTags((prevTags) => prevTags.filter((t) => t !== tag));
-    };
-
-    // end tags ----------------------------------------------------------------
 
     const onSubmit = async (data: FormSchema) => {
         if (!store) {
@@ -254,7 +130,6 @@ export default function CreatePlaylistPanel() {
 
         const playlist: Playlist = {
             ...data,
-            tags,
             id,
             createdAt,
             updatedAt,
@@ -269,9 +144,20 @@ export default function CreatePlaylistPanel() {
             // Check for duplicate ID
             const duplicate = existingPlaylists.some((p) => p.id === id);
             if (duplicate) {
-                console.log(
-                    "A playlist with this ID already exists. Aborting."
-                );
+                toast("Error", {
+                    description: "A playlist with this ID already exists.",
+                });
+                return;
+            }
+
+            // Check for duplicate playlist title
+            const duplicateTitle = existingPlaylists.some(
+                (p) => p.title === titleValue
+            );
+            if (duplicateTitle) {
+                toast("Error", {
+                    description: "A playlist with this title already exists.",
+                });
                 return;
             }
 
@@ -322,11 +208,6 @@ export default function CreatePlaylistPanel() {
                                             className="w-full border px-2 py-1 rounded"
                                             maxLength={100}
                                         />
-                                        {errors.title && (
-                                            <div className="text-red-500 text-xs mt-1">
-                                                {errors.title.message}
-                                            </div>
-                                        )}
                                     </div>
 
                                     {/* Description */}
@@ -339,117 +220,24 @@ export default function CreatePlaylistPanel() {
                                             className="w-full border px-2 py-1 rounded"
                                             maxLength={100}
                                         />
-                                        {errors.description && (
-                                            <div className="text-red-500 text-xs mt-1">
-                                                {errors.description.message}
-                                            </div>
-                                        )}
                                     </div>
 
-                                    <div className="col-span-1">
+                                    <div className="col-span-3">
                                         {/* Tags Input */}
-                                        <div>
-                                            <label className="block text-sm font-medium mb-1">
-                                                Tags
-                                            </label>
-                                            <div className="flex gap-2 mb-2">
-                                                <Input
-                                                    type="text"
-                                                    autoCapitalize="off"
-                                                    autoComplete="off"
-                                                    autoCorrect="off"
-                                                    value={tagInput}
-                                                    onChange={(e) =>
-                                                        setTagInput(
-                                                            e.target.value
-                                                        )
-                                                    }
-                                                    onKeyDown={(e) => {
-                                                        if (
-                                                            e.key === "Enter" &&
-                                                            tagInput.trim()
-                                                        ) {
-                                                            addTag(e); // Call addTag function when Enter is pressed
+                                        <TagInput
+                                            initialTags={getValues("tags")} // Initial tags from form state
+                                            onChange={
+                                                (updatedTags) =>
+                                                    setValue(
+                                                        "tags",
+                                                        updatedTags,
+                                                        {
+                                                            shouldValidate:
+                                                                false,
                                                         }
-                                                    }}
-                                                    placeholder="Add a tag"
-                                                    className="flex-1"
-                                                    disabled={
-                                                        tags.length >= MAX_TAGS
-                                                    } // Disable input if max tags reached
-                                                />
-                                                <Button
-                                                    type="button"
-                                                    size="sm"
-                                                    variant="secondary"
-                                                    onClick={addTag}
-                                                    disabled={
-                                                        tags.length >=
-                                                            MAX_TAGS || // Max tags reached
-                                                        tagInput.trim()
-                                                            .length === 0 || // Empty input
-                                                        tagInput.trim().length >
-                                                            MAX_TAG_LENGTH || // Exceeds max length
-                                                        !/^[a-zA-Z0-9]*$/.test(
-                                                            tagInput.trim()
-                                                        ) // Contains invalid characters
-                                                    }
-                                                >
-                                                    Add
-                                                </Button>
-                                            </div>
-                                            {tagInput.trim().length >
-                                                MAX_TAG_LENGTH && (
-                                                <p className="text-red-500 text-xs">
-                                                    Tags cannot exceed{" "}
-                                                    {MAX_TAG_LENGTH} characters.
-                                                </p>
-                                            )}
-                                            {tags.length === MAX_TAGS && (
-                                                <p className="text-red-500 text-xs">
-                                                    Max tags reached.
-                                                </p>
-                                            )}
-                                            {tagInput.trim().length > 0 &&
-                                                !/^[a-zA-Z0-9]*$/.test(
-                                                    tagInput
-                                                ) && (
-                                                    <p className="text-red-500 text-xs">
-                                                        Tags can only contain
-                                                        letters and numbers.
-                                                    </p>
-                                                )}
-                                        </div>
-                                    </div>
-
-                                    <div className="col-span-2">
-                                        {/* Tags Display */}
-                                        <div className="flex flex-wrap gap-2 mb-4">
-                                            {tags.map((tag, index) => (
-                                                <Badge
-                                                    key={index}
-                                                    className="flex items-center space-x-1"
-                                                    size={"sm"}
-                                                    variant={"default"}
-                                                >
-                                                    <span
-                                                        className="truncate max-w-[80px]"
-                                                        title={tag}
-                                                    >
-                                                        {tag}
-                                                    </span>
-                                                    <button
-                                                        type="button"
-                                                        onClick={() =>
-                                                            removeTag(tag)
-                                                        }
-                                                        className="ml-1"
-                                                    >
-                                                        <XIcon size={14} />
-                                                    </button>
-                                                </Badge>
-                                            ))}
-                                        </div>
+                                                    ) // Update form state when tags change
+                                            }
+                                        />
                                     </div>
 
                                     <div className="col-span-3">
@@ -536,22 +324,26 @@ export default function CreatePlaylistPanel() {
 
                             {/* Platlist Art */}
                             <div className="flex justify-center items-center relative">
-                                {selectedImage || base64Picture ? (
+                                {getValues("picture") || selectedImage ? (
                                     <img
-                                        src={selectedImage || base64Picture} // Update the image source
+                                        src={
+                                            getValues("picture") ||
+                                            selectedImage ||
+                                            undefined
+                                        }
                                         alt="Playlist Art"
                                         className="w-full h-full max-w-sm max-h-sm object-contain rounded-lg shadow cursor-pointer"
-                                        onClick={handleImageSelect} // Handle image selection
+                                        onClick={handleImageUpload}
                                     />
                                 ) : (
-                                    <div className="w-full h-full max-w-sm max-h-sm flex items-center justify-center bg-gray-100 min-h-44 max-h-96 text-gray-400 rounded-lg">
+                                    <div className="w-full h-full max-w-sm max-h-sm min-h-44 max-h-96 flex items-center justify-center bg-gray-100 text-gray-400 rounded-lg">
                                         No Playlist Art
                                     </div>
                                 )}
                                 <div className="absolute top-0 right-0 bg-black bg-opacity-50 text-white p-1 rounded-full cursor-pointer">
                                     <EditIcon
                                         size={20}
-                                        onClick={handleImageSelect} // Open file dialog on click
+                                        onClick={handleImageUpload}
                                     />
                                 </div>
                             </div>
